b1134fb6c6b8b30695116fa7f934e5ea7b8f792c
AdityaMitti <45398989+AdityaMitti@users.noreply.github.com>
2018-11-27 12:41:44 -0500

    first method
    after the first method was completed

--- githubexample.java
+++ githubexample.java
@@ -1,5 +1,16 @@
-public class githubarraylist{
+import java.util.ArrayList;
+public class githubexample{
+	public static ArrayList<Integer> combine(ArrayList<Integer> s, ArrayList<Integer> t){
+		ArrayList<Integer> n = new ArrayList<>();
+		for(int i = 0;i < s.size(); i++){
+			n.add(s.get(i));
+		}
+		for(int i = 0;i < t.size(); i++){
+			n.add(t.get(i));
+		}
+		return n;
+	}
 	public static void main(String[] args){
-	
+
 	}
 }
